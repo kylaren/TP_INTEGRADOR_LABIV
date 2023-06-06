06aa40b421921717c0ec9c7abf8ae553b3fb6312
kylaren <86126109+kylaren@users.noreply.github.com>
2023-06-06 18:02:41 -0300

    desarrollo clase Especialidad
    constructores, getters y setterd y el método toString

--- TP_CLINICA_MEDICA/src/dominio/Especialidad.java
+++ TP_CLINICA_MEDICA/src/dominio/Especialidad.java
@@ -2,4 +2,53 @@ package dominio;
 
 public class Especialidad {
 
+	private int idEspecialidad;
+	private String nombreEspecialidad;
+	private String descripcionEspecialidad;
+	
+	public Especialidad()
+	{
+		
+	}
+	
+	public Especialidad(int idEspecialidad, String nombreEspecialidad, String descripcionEspecialidad) {
+		super();
+		this.idEspecialidad = idEspecialidad;
+		this.nombreEspecialidad = nombreEspecialidad;
+		this.descripcionEspecialidad = descripcionEspecialidad;
+	}
+
+	public int getIdEspecialidad() {
+		return idEspecialidad;
+	}
+
+	public void setIdEspecialidad(int idEspecialidad) {
+		this.idEspecialidad = idEspecialidad;
+	}
+
+	public String getNombreEspecialidad() {
+		return nombreEspecialidad;
+	}
+
+	public void setNombreEspecialidad(String nombreEspecialidad) {
+		this.nombreEspecialidad = nombreEspecialidad;
+	}
+
+	public String getDescripcionEspecialidad() {
+		return descripcionEspecialidad;
+	}
+
+	public void setDescripcionEspecialidad(String descripcionEspecialidad) {
+		this.descripcionEspecialidad = descripcionEspecialidad;
+	}
+
+	@Override
+	public String toString() {
+		return "Especialidad [idEspecialidad=" + idEspecialidad + ", nombreEspecialidad=" + nombreEspecialidad
+				+ ", descripcionEspecialidad=" + descripcionEspecialidad + "]";
+	}
+	
+	
+	
+	
 }
