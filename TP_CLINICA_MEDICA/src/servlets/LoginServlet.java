6e2ee203551786676c2ac923d8ee4d182a174362
kylaren <86126109+kylaren@users.noreply.github.com>
2023-06-10 19:31:45 -0300

    creo servlet de inicio de sesión de la clínica

---
+++ TP_CLINICA_MEDICA/src/servlets/LoginServlet.java
@@ -0,0 +1,52 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+    public LoginServlet() {
+        super();
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String usuario = request.getParameter("usuario");
+        String contrasena = request.getParameter("contrasena");
+        String tipoUsuario = request.getParameter("tipoUsuario");
+
+        
+        if (usuario.equals("admin") && contrasena.equals("admin123") && tipoUsuario.equals("admin")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("tipoUsuario", "admin");
+            response.sendRedirect("admin.jsp"); // Redirige a página de administrador
+        }
+        else if (usuario.equals("medico") && contrasena.equals("medico123") && tipoUsuario.equals("medico")) {
+            HttpSession session = request.getSession();
+            session.setAttribute("tipoUsuario", "medico");
+            response.sendRedirect("medico.jsp"); // Redirige a página de médico
+        }
+        else {
+            response.sendRedirect("error.jsp");
+        }
+		
+		doGet(request, response);
+	}
+
+}
