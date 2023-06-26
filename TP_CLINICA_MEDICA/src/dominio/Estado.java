8300682396f34467f58c1eb1efe697f10ff905c6
kylaren <86126109+kylaren@users.noreply.github.com>
2023-06-26 19:17:17 -0300

    create clase Estado

---
+++ TP_CLINICA_MEDICA/src/dominio/Estado.java
@@ -0,0 +1,44 @@
+package dominio;
+
+public abstract class Estado {
+
+	private int id;
+	private String nombre;
+	
+	public Estado() 
+	{
+		
+	}
+
+	// constructor
+	public Estado(int id, String nombre) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+	}
+
+	// getters and setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	// toString 
+	@Override
+	public String toString() {
+		return "Estado [id=" + id + ", nombre=" + nombre + "]";
+	}
+	
+	
+}
